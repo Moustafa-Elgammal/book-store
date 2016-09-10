b8e3da8b60665a89e84d5dea346b68ab4b3303f9
Moustafa Elgammal <moustafa_algammal@yahoo.com>
2016-09-10 18:01:38 +0200

    Books new methods

--- includes/models/BooksModel.php
+++ includes/models/BooksModel.php
@@ -22,4 +22,47 @@ class BooksModel {
         }
         return $books;
     }
+
+    /**
+     * get a book by  id
+     * @param $id
+     * @return array of the book data or empty
+     */
+    public function GetById($id){
+        $id = (int)$id;
+
+        $book = $this->GetAllBooks("WHERE book_id = $id");
+        return !empty($book)? $book[0]: array();
+    }
+
+    /**
+     * Delete a book by it's id
+     * @param $book_id
+     * @return bool true when delete,else false
+     */
+    public function DeleteBook($book_id){
+        $id = (int)$book_id; //init
+        System::Get('db')->Delete($this->table_name,"WHERE book_id = $id"); // delete
+        $x = System::Get('db')->AffectedRows(); //check
+        return $x ? true:false; //confirm
+    }
+
+    /**
+     * @param $data
+     * @return bool true if added |else false
+     */
+    public function AddNewBook($data){
+        System::Get('db')->Insert($this->table_name,$data); // add
+        $x = System::Get('db')->AffectedRows(); //check
+        return $x? true:false;  //confirm
+    }
+
+    public function UpdateBook($id,$data){
+        $id = (int) $id; //init
+        System::Get('db')->Update($this->table_name,$data,"WHERE book_id = '$id'"); //update
+        $x = System::Get('db')->AffectedRows(); //check
+        return $x? true:false;  //confirm
+    }
+
+
 }
