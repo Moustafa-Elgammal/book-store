ef534f79c8992787561fd0fb1d070ef8038bdf15
moustafa_algammal@yahoo.com <moustafa_algammal@yahoo.com>
2016-09-12 21:20:24 +0200

    update users model

--- includes/models/UsersModel.php
+++ includes/models/UsersModel.php
@@ -2,19 +2,32 @@
 class UsersModel {
 	// init
 	private $UserInfo;
-	
-	/**
+
+    /**
+     * add new user
+     * @param  $data new user info
+     * @return boolean true if the user added
+     */
+    public function Add($data)
+    {
+        if(System::Get('db')->Insert("users",$data))
+            return TRUE;
+        return FALSE;
+    }
+
+
+    /**
 	 * get all user from db
-	 * 
-	 * @param string $extra        	
+	 *
+	 * @param string $extra
 	 * @return boolean $users else FALSE
 	 */
 	public function Get($extra = "") {
 		$users = array (); // init
-		                   
+
 		// get all users from db
 		System::Get ( 'db' )->Execute ( "SELECT*FROM `users` {$extra}" );
-		
+
 		// get users if there are;
 		if (System::Get ( 'db' )->AffectedRows ()) {
 			$users = System::Get ( 'db' )->GetRows ();
@@ -23,11 +36,11 @@ class UsersModel {
 			return FALSE;
 		}
 	}
-	
+
 	/**
 	 * Get one User info by
-	 * 
-	 * @param user $uid        	
+	 *
+	 * @param user $uid
 	 * @return Ambigous <>|boolean the user if it is found
 	 */
 	public function Get_By_ID($uid) {
@@ -38,7 +51,23 @@ class UsersModel {
 		}
 		return FALSE;
 	}
-	
+
+
+    /**
+     * get the admin users
+     * @return unknown|boolean
+     */
+    public function Get_By_Admin()
+    {
+
+        $users = $this->Get("WHERE `is_admin`=1");
+        if(count($users) > 0)
+        {
+            return $users;
+        }
+        return FALSE;
+    }
+
 	/**
 	 * check the user info with its email or user name
 	 */
@@ -53,10 +82,10 @@ class UsersModel {
 		}
 		return FALSE;
 	}
-	
+
 	/**
 	 * update user
-	 * 
+	 *
 	 * @param
 	 *        	$id
 	 * @param $data info
@@ -70,10 +99,10 @@ class UsersModel {
 		}
 		return FALSE;
 	}
-	
+
 	/**
 	 * reset user password by
-	 * 
+	 *
 	 * @param
 	 *        	$id
 	 * @param $password new
@@ -82,15 +111,15 @@ class UsersModel {
 	 */
 	public function Reset_password($uid, $password) {
 		if (System::Get ( 'db' )->Update ( "users", array (
-				'password' => $password 
+				'password' => $password
 		), "WHERE `users`.`uid`=('$uid')" ))
 			return TRUE;
-		
+
 		return FALSE;
 	}
 	/**
 	 * Login user by
-	 * 
+	 *
 	 * @param
 	 *        	$username
 	 * @param
@@ -100,7 +129,7 @@ class UsersModel {
 	public function Login($email_or_username, $password) {
 		$email_or_username = System::SQL_SAVE($email_or_username);
 		$password = System::Hash ( $password );
-		
+
 		if ($this->Get ( "WHERE `users`.`email`=('$email_or_username') OR `users`.`username`=('$email_or_username') AND `password`=('$password')" ) && $password) {
 			$user = $this->Get ( "WHERE `users`.`email`=('$email_or_username') OR `users`.`username`=('$email_or_username') AND `password`=('$password')" );
 			$this->UserInfo = $user [0];
@@ -111,7 +140,24 @@ class UsersModel {
 			return FALSE;
 		}
 	}
-	
+
+    /**
+     * delete user by
+     * @param  $uid
+     * @return boolean TRUE if user DELETED
+     */
+    public function Delete($uid)
+    {
+        $uid=(int)$uid;//casting
+
+        //delete
+        if(System::Get('db')->Delete('users',"WHERE `uid`=$uid"))
+            return TRUE;
+
+        return FALSE;
+
+    }
+
 	/**
 	 *
 	 * @return the User Info
