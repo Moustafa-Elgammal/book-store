7d718df936be8b98b1edd59afdc0f92561f00f14
moustafa_algammal@yahoo.com <moustafa_algammal@yahoo.com>
2016-09-13 12:14:39 +0200

    show all categories

---
+++ admin/categories.php
@@ -0,0 +1,11 @@
+<?php
+require_once"../globals.php";
+Is_Admin();
+
+require_once(MODELS."/CategoriesModel.php");
+require_once(CONTROLLERS."/CategoriesController.php");
+
+$model = new CategoriesModel();
+$controller = new CategoriesController($model);
+
+$controller->AllCategories();
