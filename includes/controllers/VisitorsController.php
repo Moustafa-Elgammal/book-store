cccd17c3d8bb1f52b5794bbb04dc010b0d5a85ac
moustafa_algammal@yahoo.com <moustafa_algammal@yahoo.com>
2016-09-16 02:12:23 +0200

    show single book

--- includes/controllers/VisitorsController.php
+++ includes/controllers/VisitorsController.php
@@ -16,4 +16,23 @@ class VisitorsController {
         System::Get('tpl')->draw('home');
         System::Get('tpl')->draw('footer');
     }
+
+    public function drawBookInfo(){
+        if(isset($_GET['id']) && (int)$_GET['id'] ){
+            $id = (int)$_GET['id'];
+
+            $book = $this->booksModel->GetById($id);
+
+            if(!empty($book)){
+                System::Get('tpl')->assign($book);
+                System::Get('tpl')->draw('header');
+                System::Get('tpl')->draw('book');
+                System::Get('tpl')->draw('footer');
+            } else{
+                $this->drawHome();
+            }
+        }else{
+            $this->drawHome();
+        }
+    }
 } 
--- templates/assets/js/app.js
+++ templates/assets/js/app.js
@@ -1,3 +1,15 @@
 /**
  * Created by tone on 16/09/2016.
  */
+jQuery(document).ready(function($){
+    $('.download').click(function(){
+        var file = $(this).attr('file-data');
+        window.location.href = file;
+    });
+
+
+    $('.view-more').click(function(){
+        var file = 'book.php?id='+$(this).attr('data-id');
+        window.location.href = file;
+    });
+});
