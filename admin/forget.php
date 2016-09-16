af4bea4397fca5174c4115b8760ae3f88b0495eb
Moustafa Elgammal <moustafa_algammal@yahoo.com>
2016-09-16 15:41:27 +0200

    disable forget password

--- admin/forget.php
+++ admin/forget.php
@@ -2,7 +2,7 @@
 require_once('../globals.php');
 require_once (CONTROLLERS.'UsersController.php');
 require_once (MODELS.'UsersModel.php');
-
+die('<h1 align="center">Not supported @ localhost</h1><p align="center"><a href="../../">home</a></p>');
 $UsersModel=new UsersModel();
 
 $Controller = new UsersController($UsersModel);
