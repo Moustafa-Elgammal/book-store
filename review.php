52f9342cf76490aa4042f4733e24be95b0f0a5ea
Moustafa Elgammal <moustafa_algammal@yahoo.com>
2016-09-17 11:54:28 +0200

    review route

---
+++ review.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once"globals.php";
+require_once(CONTROLLERS.'ReviewsController.php');
+require_once(MODELS.'ReviewsModel.php');
+
+$model = new ReviewsModel();
+$controller = new ReviewsController($model);
+
+$controller->AddReview();
+
