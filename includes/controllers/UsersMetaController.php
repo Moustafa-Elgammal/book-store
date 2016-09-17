7edd505473b880bb3e9e5a6c495656d91f991309
Moustafa Elgammal <moustafa_algammal@yahoo.com>
2016-09-17 09:54:31 +0200

    methods for download and read the book for the users

--- includes/controllers/UsersMetaController.php
+++ includes/controllers/UsersMetaController.php
@@ -5,7 +5,8 @@
  * Date: 9/16/2016
  * Time: 4:15 PM
  */
-
+define('WANT_TO_READ','1');
+define('DOWNLOADED','2');
 class UsersMetaController {
     private $MetaModel;
 
@@ -13,22 +14,82 @@ class UsersMetaController {
         $this->MetaModel = $model;
     }
 
-    public function addReview(){
+    public function WantToRead(){
         if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-            if ($_SESSION['uid'])
+            if (!$_SESSION['uid'])
                 die(json_encode(array(
                     'status' => 0,
                     'msg' => 'you Must login first'
                 )));
 
-            if (!$_POST['book_id'])
+            if (!(int)$_POST['book_id'])
                 die(json_encode(array(
                     'status' => 0,
                     'msg' => 'please refresh this page'
                 )));
-            $data =array(
+            if(!$this->MetaModel->CheckExist((int)$_POST['book_id'],(int)$_SESSION['uid'],WANT_TO_READ))
+                die(json_encode(array(
+                    'status' => 0,
+                    'msg' => 'You have read this book before'
+                )));
 
+            $data =array(
+                'meta_book_id'=>(int)$_POST['book_id'],
+                'meta_user_id'=>(int)$_SESSION['uid'],
+                'meta_type'=>WANT_TO_READ
             );
+
+            $x = $this->MetaModel->AddNewMeta($data);
+            $x?die(json_encode(array(
+                'status' => 1,
+                'msg' => 'Successfully Read'
+            ))):die(json_encode(array(
+                'status' => 0,
+                'msg' => 'cant add now'
+            )));
+
+        }else{
+            die(json_encode(array(
+                'status' => 0,
+                'msg' => 'error'
+            )));
+        }
+
+    }
+
+    public function DownloadBook(){
+        if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            if (!$_SESSION['uid'])
+                die(json_encode(array(
+                    'status' => 0,
+                    'msg' => 'you Must login first'
+                )));
+
+            if (!(int)$_POST['book_id'])
+                die(json_encode(array(
+                    'status' => 0,
+                    'msg' => 'please refresh this page'
+                )));
+            if(!$this->MetaModel->CheckExist((int)$_POST['book_id'],(int)$_SESSION['uid'],DOWNLOADED))
+                die(json_encode(array(
+                    'status' => 0,
+                    'msg' => 'You have downloaded this book before'
+                )));
+
+            $data =array(
+                'meta_book_id'=>(int)$_POST['book_id'],
+                'meta_user_id'=>(int)$_SESSION['uid'],
+                'meta_type'=>DOWNLOADED
+            );
+
+            $x = $this->MetaModel->AddNewMeta($data);
+            $x?die(json_encode(array(
+                'status' => 1,
+                'msg' => 'Successfully Read'
+            ))):die(json_encode(array(
+                'status' => 0,
+                'msg' => 'cant add now'
+            )));
 
         }else{
             die(json_encode(array(
