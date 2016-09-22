ffd29080c6d541a87c4adff725ff79b9a6f3382b
Moustafa Elgammal <moustafa_algammal@yahoo.com>
2016-09-22 09:14:35 +0200

    author route for his books

---
+++ admin/books.Author.php
@@ -0,0 +1,11 @@
+<?php
+require_once("../globals.php");
+Is_Author();
+
+require_once(CONTROLLERS.'BooksController.php');
+require_once(MODELS.'BooksModel.php');
+
+$model = new BooksModel();
+$controller = new BooksControllers($model);
+
+$controller->Author_books();
