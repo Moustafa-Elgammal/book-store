0c98d052d8a6bbe9bb7c2c2412fa3b20df0b599e
Moustafa Elgammal <moustafa_algammal@yahoo.com>
2016-09-22 09:38:59 +0200

    disable Author function for a moment

--- includes/general.php
+++ includes/general.php
@@ -1,15 +1,36 @@
 <?php
 
-/**
- * to check if there is an admine user is log in
- */
-function Is_Admin() {
-	if (! isset ( $_SESSION ['is_admin'] ) || $_SESSION ['is_admin'] < 1) {
-		System::RedirectTo ( 'login.php' );
-	}
-	/*
-	 * file which assign all the inportant info for the recent user
-	 */
-	require 'now.php';
+function Author(){
+//    if (isset ( $_SESSION ['is_admin'] ) && $_SESSION ['is_admin'] > 1) {
+//        System::RedirectTo ( 'index.author.php' );
+//    }
 }
-?>
+
+/**
+ * to check if there is an admin user is log in
+ */
+
+function Is_Admin() {
+    Author();
+    if (! isset ( $_SESSION ['is_admin'] ) || $_SESSION ['is_admin'] < 1) {
+        System::RedirectTo ( 'login.php' );
+    }
+    /*
+     * file which assign all the important info for the recent user
+     */
+    require 'now.php';
+}
+
+/**
+ * to check if there is an Author user is log in
+ */
+function Is_Author() {
+    if (! isset ( $_SESSION ['is_admin'] ) || $_SESSION ['is_admin'] != 2) {
+        System::RedirectTo ( 'login.php' );
+    }
+    /*
+     * file which assign all the important info for the recent user
+     */
+    require 'now.php';
+}
+
