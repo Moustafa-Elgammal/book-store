a9a77462ce8db24d570d595d1cb3b3b61ed24500
Moustafa Elgammal <moustafa_algammal@yahoo.com>
2016-09-23 15:38:31 +0200

    get the Author info and books

--- author.php
+++ author.php
@@ -1,1 +1,11 @@
 <?php
+require_once('globals.php');
+require_once(MODELS.'BooksModel.php');
+require_once(CONTROLLERS.'UsersController.php');
+require_once(MODELS.'UsersModel.php');
+$book= new BooksModel();
+$model = new UsersModel();
+$controller = new UsersController($model);
+
+$controller->GetAuthor($book);
+
