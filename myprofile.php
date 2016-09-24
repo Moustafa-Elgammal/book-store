062ef237350df9213d0f9d9cc38437c5eda1c37d
Moustafa Elgammal <moustafa_algammal@yahoo.com>
2016-09-24 12:52:25 +0200

    user profile update

--- myprofile.php
+++ myprofile.php
@@ -1,1 +1,13 @@
 <?php
+include_once"globals.php";
+
+isset($_SESSION['uid'])?:System::RedirectTo('admin/login.php');
+
+require_once(CONTROLLERS.'UsersMetaController.php');
+require_once(MODELS.'UsersMetaModel.php');
+
+$model = new UsersMetaModel();
+$controller = new UsersMetaController($model);
+
+$controller->UserProfile();
+
