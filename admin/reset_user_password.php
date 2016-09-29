daf16b34dddd1bd11ce77f63dcb023a00f424129
Moustafa Elgammal <moustafa_algammal@yahoo.com>
2016-09-29 11:58:46 +0200

    reset password route

--- admin/reset_user_password.php
+++ admin/reset_user_password.php
@@ -1,1 +1,11 @@
 <?php
+require_once('../globals.php');
+require_once (CONTROLLERS.'UsersController.php');
+require_once (MODELS.'UsersModel.php');
+
+$UsersModel=new UsersModel();
+
+$Controller = new UsersController($UsersModel);
+
+
+$Controller->ResetPassword();
