959effede6f4d29bec344f19407f8f76c9f884a6
Moustafa Elgammal <moustafa_algammal@yahoo.com>
2016-09-29 11:24:36 +0200

    user update info route

---
+++ admin/updateinfo.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('../globals.php');
+require_once (CONTROLLERS.'UsersController.php');
+require_once (MODELS.'UsersModel.php');
+
+$UsersModel=new UsersModel();
+
+$Controller = new UsersController($UsersModel);
+
+
+$Controller->updateInfo();
